7664917520b12b7ec9cc944d039412a78bb3cd15
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-17 16:10:50 +0200

    2-binary_tree_insert_right.c

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+  *
+ * @parent: parent node
+ * @value: data
+ * Return: binary_tree_t*
+ */
+
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (!parent)
+		return (NULL);
+
+	new_node = binary_tree_node(parent, value);
+
+	if (!new_node)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
+	parent->right = new_node;
+	return (new_node);
+}
