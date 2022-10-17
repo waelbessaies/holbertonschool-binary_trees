11d742452a944557b2a16f7062994d569d4cde68
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-17 19:38:15 +0200

    post-order traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,17 +1,17 @@
 #include "binary_trees.h"
 /**
-* binary_tree_inorder - post-order traversal
-*
-* @tree:  binary tree
-* @func: function
-* Return: nothing
-*/
+ * binary_tree_postorder -  postorder traversal
+ *
+ * @tree: binary tree
+ * @func: function 
+ * Return: nothing
+ */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 if (tree == NULL || func == NULL)
 return;
 
-binary_tree_inorder(tree->left, func);
-binary_tree_inorder(tree->right, func);
+binary_tree_postorder(tree->left, func);
+binary_tree_postorder(tree->right, func);
 func(tree->n);
 }
