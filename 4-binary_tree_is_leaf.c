6bcc6d9ba6d026eea10b0843f3d7771c95516a3d
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-17 17:23:31 +0200

    checks if a node is a leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,7 +9,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 if (node == NULL)
 return (0);
 
-if (node->left == NULL || node->right == NULL)
+if (node->left  || node->right)
 return (0);
 
 return (1);
