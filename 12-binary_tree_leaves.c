0a11c4deefa942094e5bd27d0c15d308afad29c2
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-27 00:11:35 +0200

    12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - count leaves
+ * @tree: tree
+ * Return: value
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+
+	if (tree == NULL)
+		return (0);
+	if (!(tree->left && tree->right))
+	    return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
