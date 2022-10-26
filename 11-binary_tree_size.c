4dc917ea55ffe5faca25135560f66bcee7131b73
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-26 23:40:41 +0200

    11-binary_tree_size.c

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - size of a binary tree
+ * @tree: root node 
+ * Return: the size of the binary tree.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return ((binary_tree_size(tree->left) + binary_tree_size(tree->right)) + 1);
+}
