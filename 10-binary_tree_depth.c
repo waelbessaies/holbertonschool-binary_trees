8cacf4c335905a1d73ccba28d69a86ca89c02003
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-26 23:12:36 +0200

    10-binary_tree_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * binary_tree_depth -  measures the depth of  tree
+ * @tree: pointer to the root node of the tree to traverse
+ * Return: Depth of the binary tree, 0 if none
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->parent != NULL)
+		return (binary_tree_depth(tree->parent) + 1);
+	else
+		return (0);
+
+}
