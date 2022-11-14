af0a488d4fed15e3d413e98e9a66ec799083d098
root <root@LAPTOP-I8GVERUB.localdomain>
2022-11-14 14:09:30 +0100

    finds the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,8 +1,7 @@
 #include "binary_trees.h"
 /**
  * binary_tree_uncle - finds the uncle of a node
- * @node: a pointer to the node 
- *
+ * @node: a pointer to the node
  * Return: pointer to the uncle node
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
